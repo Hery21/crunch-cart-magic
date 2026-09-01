071c87884d81e5f88feda6c271d8c0fc1a58ce54
Hery <22.Hery86@gmail.com>
2026-09-02 02:25:13 +0700

    refactor: change the sheets

--- lib/pos-types.ts
+++ lib/pos-types.ts
@@ -105,7 +105,7 @@ export const DEFAULT_SETTINGS: Settings = {
   prices: DEFAULT_PRICES,
   pin: "1234",
   sheetsEndpoint:
-    "https://script.google.com/macros/s/AKfycbzHgKBZsTk3E6-0Edcvu3q8oUeGT1gnsD10ChYFYoXlXWWlcEpNCi8U_JdgO1ofJGSKKQ/exec",
+    "https://script.google.com/macros/s/AKfycbxMWByFItQI3nkJp2Uc02ATxH63wdPnACj4AnioRy1hjQOsf0EucBggiO_ILMLTSGBZ/exec",
   invoiceCounter: 0,
 };
 
